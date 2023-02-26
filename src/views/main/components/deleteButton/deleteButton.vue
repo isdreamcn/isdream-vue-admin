4aba4c8ff5943df58925d75277a371f2623d1409
马天梦 <2541817610@qq.com>
2023-02-26 20:58:46 +0800

    perf(views): delete-button 使用mock api

--- src/views/main/components/deleteButton/deleteButton.vue
+++ src/views/main/components/deleteButton/deleteButton.vue
@@ -2,7 +2,7 @@
   <div>
     <p>批量删除按钮</p>
     <MDeleteButton
-      :http="deleteHttp"
+      :http="demoUserDel"
       :selectKeys="selectKeys"
       @click="reload"
       @reload="reload"
@@ -18,21 +18,13 @@
 
 <script setup lang="ts">
 import readme from '@/components/deleteButton/README.md?raw'
+import { demoUserDel } from '@/api/demo/user'
 
 defineOptions({
   name: 'DemoCpnsDeleteButton'
 })
 
 const selectKeys = [1, 2, 3, 4, 5]
-
-const deleteHttp = (id: number | string) => {
-  console.log('删除', id)
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({})
-    }, 2000)
-  })
-}
 
 const reload = () => {
   console.log('reload')
