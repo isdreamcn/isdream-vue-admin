86cb48d56b8132b0b738d6fe1757cc572fafd152
马天梦 <2541817610@qq.com>
2023-10-11 15:58:09 +0800

    perf(build): 增加注释说明

--- build/vite/plugins/rollupVisualizer.ts
+++ build/vite/plugins/rollupVisualizer.ts
@@ -1,4 +1,7 @@
 // TODO: https://github.com/btd/rollup-plugin-visualizer
+// 依赖分析 (stats.html)
+// 可视化并分析您的 Rollup 包，分析项目中的文件大小及引用情况
+
 import { visualizer } from 'rollup-plugin-visualizer'
 
 export const rollupVisualizer = () => visualizer()
--- build/vite/plugins/useGzip.ts
+++ build/vite/plugins/useGzip.ts
@@ -1,6 +1,13 @@
 // TODO: https://github.com/vbenjs/vite-plugin-compression
 import commpressPlugin from 'vite-plugin-compression'
 
+/*
+  静态压缩
+  在客户端替nginx处理压缩文件这一步操作，nginx便可直接使用我们压缩好的文件，尽可能减少对服务端内存的使用
+
+  保证Nginx存在`ngx_http_gzip_static_module`模块, 一般配置:
+  gzip_static on;
+*/
 export const useGzip = () =>
   commpressPlugin({
     ext: '.gz',
