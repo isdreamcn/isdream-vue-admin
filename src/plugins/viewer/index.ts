196814c93c126c72553175ef90fc262162a8ce47
isdream <2541817610@qq.com>
2025-06-28 22:21:05 +0800

    fix(plugins): v-viewer z-index小于 el-dialog

--- src/plugins/viewer/index.ts
+++ src/plugins/viewer/index.ts
@@ -2,5 +2,9 @@ import type { AppUsePlugin } from '../types'
 import VueViewer from 'v-viewer'
 
 export const useViewer: AppUsePlugin = (app) => {
-  app.use(VueViewer)
+  app.use(VueViewer, {
+    defaultOptions: {
+      zIndex: 9999
+    }
+  })
 }
